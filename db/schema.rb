db1c6c5eba7634f24d561b1b1f43f22f5d9e45cd
Marvki <marvkipi@gmail.com>
2017-08-24 08:50:51 -0700

    nested forms 15%

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170821062437) do
+ActiveRecord::Schema.define(version: 20170824154936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,13 @@ ActiveRecord::Schema.define(version: 20170821062437) do
     t.integer  "account_id"
     t.integer  "user_id"
     t.index ["author_id"], name: "index_projects_on_author_id", using: :btree
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "value"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "subscription_events", force: :cascade do |t|
