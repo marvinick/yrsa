781467132eda7d4cd0285d43e8c26134f8f6e410
Marvin Nikijuluw <marvkipi@gmail.com>
2017-10-06 23:19:58 -0700

    heroku user_id?

--- db/migrate/20171003154406_add_user_id_to_reviews.rb
+++ db/migrate/20171003154406_add_user_id_to_reviews.rb
@@ -1,5 +1,5 @@
 class AddUserIdToReviews < ActiveRecord::Migration[5.0]
   def change
-    add_column :reviews, :user_id, :integer
+    add_column :reviews, :user_id, :integer, foreign_key: true
   end
 end
