4fcaf7368209e6bfd99d03fdd0e4001205607d18
Marvin Nikijuluw <marvkipi@gmail.com>
2017-10-10 22:17:42 -0700

    bootstrap rewrite

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,24 +1,4 @@
 module ProjectsHelper
 
-  def project_item_average_rating
-    project_item_rating
-  end
-
-  def project_item_rating
-    tar = []
-    total_average_ratings = []
-    total_rv = []
-    @project.items.each do |item|
-      item.details.each do |detail|
-        detail.reviews.each do |review|
-          total_rv << review.value
-          total_average_ratings = total_rv.sum / total_rv.count
-        end
-        tar << total_average_ratings
-      end
-      tar.sum
-    end
-    tar
-  end
 
 end
