1326b8e9b928dd8243d8686490d7aa9d81256b5a
Marvin Nikijuluw <marvkipi@gmail.com>
2017-10-14 20:04:18 -0700

    add image column to item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171003154406) do
+ActiveRecord::Schema.define(version: 20171015030159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,8 +51,12 @@ ActiveRecord::Schema.define(version: 20171003154406) do
     t.string   "name"
     t.text     "content"
     t.integer  "project_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "memberships", force: :cascade do |t|
