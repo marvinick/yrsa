5571afed276ace8759266ff449aae0bfe56de08b
Marvin Nikijuluw <marvkipi@gmail.com>
2017-10-14 20:42:02 -0700

    aws s3 works

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,11 @@ class Item < ApplicationRecord
   belongs_to :project, optional: true
   has_many :details, dependent: :destroy
   accepts_nested_attributes_for :details, allow_destroy: true, reject_if: ->(attrs) { attrs['name'].blank? || attrs['description'].blank? }
-  has_attached_file :image
+  has_attached_file :image, styles: { small: "64x64#", med: "200x100#", large: "320x200#", xlarge: "800x600#" }
+
+  validates_attachment_content_type :image,
+		content_type:  /^image\/(png|gif|jpeg)/,
+		message: "Only images allowed"
+
+    validates :image, attachment_presence: true
 end
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,23 @@ module Yrsa
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.action_controller.permit_all_parameters = true
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
+    # config.active_record.raise_in_transactional_callbacks = true
+    config.paperclip_defaults = {
+        :storage => :s3,
+        :s3_host_name => 's3-us-west-2.amazonaws.com',
+        :s3_region => ENV['AWS_REGION'],
+        :s3_credentials => {
+            :bucket => ENV['AWS_BUCKET'],
+            :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+            :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+        }
+    }
+
+    config.time_zone = 'Pacific Time (US & Canada)'
+    config.active_record.default_timezone = :local
   end
 end
