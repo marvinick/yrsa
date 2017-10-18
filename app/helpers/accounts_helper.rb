7204f942bbbba55cc1f496835d45aad5e2f0bfa2
Marvin Nikijuluw <marvkipi@gmail.com>
2017-10-18 08:52:36 -0700

    account layout

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,5 +1,25 @@
 module AccountsHelper
-  def project_count
+  def projects_in_account
     @account.projects.count
-  end 
+  end
+
+  def items_in_project
+    project_items = []
+    @account.projects.each do |project|
+      project_items << project.items.count
+    end
+    project_items.sum
+  end
+
+  def reviews_in_project
+    project_reviews = []
+    @account.projects.each do |project|
+      project.items.each do |item|
+        item.details.each do |detail|
+          project_reviews << detail.reviews.count
+        end
+      end
+    end
+    project_reviews.sum
+  end
 end
