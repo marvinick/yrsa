a22e7852d6770b6c26c2c35a7ce1d8dc9c2c21cb
Marvin Nikijuluw <marvkipi@gmail.com>
2017-12-16 17:01:49 -0800

    public activity display correctly

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,7 @@
 class ActivitiesController < ApplicationController
   def index
-    # @activities = PublicActivity::Activity.order("created_at DESC").where(owner_type: "User)
+    @activities = PublicActivity::Activity.order("created_at DESC").where(owner_type: "User")
     # @activities = PublicActivity::Activity.order("created_at DESC").where(trackable_type: "Review")
-    @activities = PublicActivity::Activity.all
+    # @activities = PublicActivity::Activity.all
   end
 end
