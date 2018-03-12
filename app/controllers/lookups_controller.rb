6b9de84bcfe45647d287f1b211d858c1224b23cb
Marvin Nikijuluw <marvkipi@gmail.com>
2018-03-11 22:32:37 -0700

    where filter

--- app/controllers/lookups_controller.rb
+++ app/controllers/lookups_controller.rb
@@ -14,12 +14,20 @@ class LookupsController < ApplicationController
   # end
 
   def index
+    project_ids = current_account.projects.all
+    item_ids = set_project.items.all
     respond_to do |format|
       if params[:term]
-        @pg_search_documents = PgSearch.multisearch(params[:term])
+        @pg_search_documents = PgSearch.multisearch(params[:term]).where()
       end
       format.json { @pg_search_documents }
       format.html
     end
+  end
+
+  private
+
+  def set_project
+    current_account.projects.find_by slug: params[:project_id]
   end
 end
