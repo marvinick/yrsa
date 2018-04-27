3e97b252c1b3b57614cbd5b045e5eb795d569a46
Marvin Nikijuluw <marvkipi@gmail.com>
2018-04-27 08:49:17 -0700

    shrine S3

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,5 @@
 class Photo < ApplicationRecord
   include ImageUploader[:image]
-  # has_attached_file :image, :styles => { :medium => "300x300#", :thumb => "200x200#" }
-  # validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
   
+
 end
