3e8c8900a2d39095c0dd11f6dc406bb2bfa62778
Marvin Nikijuluw <marvkipi@gmail.com>
2018-05-17 21:41:51 -0700

    payment source rescue

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -10,22 +10,19 @@ class PlansController < BaseController
     customer = Stripe::Customer.retrieve(current_account.stripe_customer_id)
     plan = Plan.find(params[:account][:plan_id])
 
-    if Stripe::InvalidRequestError
+    subscription = customer.subscriptions.create(
+      plan: plan.stripe_id,
+      source: params[:token]
+    )
+    current_account.plan_id = params[:account][:plan_id]
+    current_account.stripe_subscription_id = subscription.id
+    current_account.save
+    flash[:notice] = "Your account has been created."
+    redirect_to root_url
+
+    rescue Stripe::InvalidRequestError
       flash[:alert] = "No payment source."
       redirect_to account_billing_path(current_account)
-    else
-      subscription = customer.subscriptions.create(
-
-        plan: plan.stripe_id,
-        source: params[:token]
-      )
-      current_account.plan_id = params[:account][:plan_id]
-      current_account.stripe_subscription_id = subscription.id
-      current_account.save
-      flash[:notice] = "Your account has been created."
-      redirect_to root_url
-    end
-
   end
 
   def cancel
@@ -57,13 +54,5 @@ class PlansController < BaseController
 
     flash[:notice] = "You have changed to the #{plan.name} plan."
     redirect_to root_url
-  end
-
-  private
-
-  def has_payment_source?
-    rescue Stripe::InvalidRequestError
-
-      redirect_to account_billing_path(current_account)
   end
 end
