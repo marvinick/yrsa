1583007082293c32638268b2c4a16b93c4a550d4
Marvin Nikijuluw <marvkipi@gmail.com>
2018-07-04 22:40:11 -0700

    account view

--- app/assets/javascripts/billing.js
+++ app/assets/javascripts/billing.js
@@ -1,26 +1,26 @@
-// $(document).ready(function () {
-//   var handler = StripeCheckout.configure({
-//     key: 'pk_test_LLU9HECjsQymV98V9d9GUxLS',
-//     image: '',
-//     locale: 'auto',
-//     token: function(token) {
-//       $('form').append("<input type='hidden' name='token' value='" + token.id + "' />")
-//       $('form').submit();
-//     }
-//   });
-//
-//   $('#payment-btn').on('click', function(e) {
-//     handler.open({
-//       name: 'Update payment method',
-//       currency: "usd",
-//       email: $('#email').val(),
-//       panelLabel: "Update payment details"
-//     });
-//     e.preventDefault();
-//   });
-//
-//   // Close Checkout on page navigation
-//   $(window).on('popstate', function() {
-//     handler.close();
-//   });
-// });
+$(document).ready(function () {
+  var handler = StripeCheckout.configure({
+    key: 'pk_test_LLU9HECjsQymV98V9d9GUxLS',
+    image: '',
+    locale: 'auto',
+    token: function(token) {
+      $('form').append("<input type='hidden' name='token' value='" + token.id + "' />")
+      $('form').submit();
+    }
+  });
+
+  $('#payment-btn').on('click', function(e) {
+    handler.open({
+      name: 'Update payment method',
+      currency: "usd",
+      email: $('#email').val(),
+      panelLabel: "Update payment details"
+    });
+    e.preventDefault();
+  });
+
+  // Close Checkout on page navigation
+  $(window).on('popstate', function() {
+    handler.close();
+  });
+});
